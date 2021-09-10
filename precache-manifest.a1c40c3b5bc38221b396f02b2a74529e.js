bc0c6218c6078d6a06c58c25e329dff6730b0ef8
Nazar Dzhura <nazar_dzhura@ukr.net>
2021-09-10 11:37:39 +0300

    Updates

---
+++ precache-manifest.a1c40c3b5bc38221b396f02b2a74529e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7172dfbcb5d12baaa8504ae8432dd9",
+    "url": "/inforce/index.html"
+  },
+  {
+    "revision": "c7cdb93cac9ca6b9640e",
+    "url": "/inforce/static/css/main.2bd2d2a2.chunk.css"
+  },
+  {
+    "revision": "2c4bb20fd3eed0e714d0",
+    "url": "/inforce/static/js/2.6ba7de52.chunk.js"
+  },
+  {
+    "revision": "88acba1181dc48116778d847d6dad120",
+    "url": "/inforce/static/js/2.6ba7de52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7cdb93cac9ca6b9640e",
+    "url": "/inforce/static/js/main.3415e3c6.chunk.js"
+  },
+  {
+    "revision": "883dbcddc0d59107bb48",
+    "url": "/inforce/static/js/runtime-main.22897a43.js"
+  }
+]);
